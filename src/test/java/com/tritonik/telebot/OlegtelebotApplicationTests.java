699668af4a41903efde3347656331765bd3febc5
Oleg <ingainofficial@mail.ru>
2024-06-21 18:25:03 +0300

    я забыль
    я забыль

---
+++ src/test/java/com/tritonik/telebot/OlegtelebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tritonik.telebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlegtelebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
